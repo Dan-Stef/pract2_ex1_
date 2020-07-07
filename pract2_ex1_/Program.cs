ed1d4f2ae2baf07e4836cf6102519eb9e86a06bd
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-08 02:00:16 +0300

    Добавьте файлы проекта.

---
+++ pract2_ex1_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract2_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x=");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("y=");
+            float y = float.Parse(Console.ReadLine());
+            if (x * x + y * y < 15 * 15 && y > Math.Abs(x))
+                Console.WriteLine("Да");
+            else if (x * x + y * y <= 15 * 15 && y == Math.Abs(x) || x * x + y * y == 15 * 15 && y > Math.Abs(x))
+                Console.WriteLine("На границе");
+            else Console.WriteLine("Нет");
+            Console.ReadKey();
+        }
+    }
+}
